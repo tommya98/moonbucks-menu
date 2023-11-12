031e256dabf25729a4ab228868c3296aa30503b9
tommya98 <tommya98@naver.com>
2023-11-12 22:03:04 +0900

    FEAT: 메뉴를 입력받고 확인 버튼 또는 엔터키를 누르면 메뉴를 추가한다

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+function App() {
+  const menuForm = document.getElementById("espresso-menu-form");
+  const menuFromInput = document.getElementById("espresso-menu-name");
+  const menuList = document.getElementById("espresso-menu-list");
+
+  menuForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const menuName = menuFromInput.value;
+    const menuItem = menuItemTemplate(menuName);
+    menuList.insertAdjacentHTML("beforeend", menuItem);
+  });
+}
+
+const menuItemTemplate = (menuName) => {
+  return `
+  <li class="menu-list-item d-flex items-center py-2">
+    <span class="w-100 pl-2 menu-name">${menuName}</span>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+    >
+      수정
+    </button>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+    >
+      삭제
+    </button>
+  </li>`;
+};
+
+App();
