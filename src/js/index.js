95cc2f46f8b5f3126b5e940baeb6002503347c75
tommya98 <tommya98@naver.com>
2023-12-01 13:15:33 +0900

    FEAT: API 통신 실패 시 alert으로 예외처리를 진행한다

--- src/js/index.js
+++ src/js/index.js
@@ -4,42 +4,63 @@ const BASE_URL = "http://localhost:3000/api";
 
 const MenuApi = {
   postNewMenu: async (category, menuName) => {
-    await fetch(`${BASE_URL}/category/${category}/menu`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name: menuName }),
-    });
+    try {
+      await fetch(`${BASE_URL}/category/${category}/menu`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: menuName }),
+      });
+    } catch (error) {
+      alert(error.message);
+    }
   },
   getAllMenuByCategory: async (category) => {
-    const response = await fetch(`${BASE_URL}/category/${category}/menu`);
-    return response.json();
+    try {
+      const response = await fetch(`${BASE_URL}/category/${category}/menu`);
+      return response.json();
+    } catch (error) {
+      console.log(error.message);
+      alert(error.message);
+    }
   },
   putChnagedMenuName: async (category, menuName, menuId) => {
-    await fetch(`${BASE_URL}/category/${category}/menu/${menuId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name: menuName }),
-    });
+    try {
+      await fetch(`${BASE_URL}/category/${category}/menu/${menuId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: menuName }),
+      });
+    } catch (error) {
+      alert(error.message);
+    }
   },
   deleteMenu: async (category, menuId) => {
-    await fetch(`${BASE_URL}/category/${category}/menu/${menuId}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    try {
+      await fetch(`${BASE_URL}/category/${category}/menu/${menuId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      alert(error.message);
+    }
   },
   putMenuSoldout: async (category, menuId) => {
-    await fetch(`${BASE_URL}/category/${category}/menu/${menuId}/soldout`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    try {
+      await fetch(`${BASE_URL}/category/${category}/menu/${menuId}/soldout`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      alert(error.message);
+    }
   },
 };
 
