c67fbd77980530318b423bb59aa294e37c323680
tommya98 <tommya98@naver.com>
2023-12-01 13:49:09 +0900

    REFACTOR: api 호출 객체 추상화

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -1,64 +1,76 @@
 const BASE_URL = "http://localhost:3000/api";
 
+const HTTP_METHOD = {
+  POST(data) {
+    return {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+  },
+  PUT(data) {
+    return {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data ? JSON.stringify(data) : null,
+    };
+  },
+  DELETE() {
+    return {
+      method: "DELETE",
+    };
+  },
+};
+
+const request = async (url, option) => {
+  try {
+    const response = await fetch(url, option);
+    return await response.json();
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+const requestWithOutJson = async (url, option) => {
+  try {
+    const response = await fetch(url, option);
+    return response;
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
 const MenuApi = {
-  postNewMenu: async (category, menuName) => {
-    try {
-      await fetch(`${BASE_URL}/category/${category}/menu`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name: menuName }),
-      });
-    } catch (error) {
-      alert(error.message);
-    }
+  postNewMenu: async (category, name) => {
+    return await request(
+      `${BASE_URL}/category/${category}/menu`,
+      HTTP_METHOD.POST({ name })
+    );
   },
   getAllMenuByCategory: async (category) => {
-    try {
-      const response = await fetch(`${BASE_URL}/category/${category}/menu`);
-      return response.json();
-    } catch (error) {
-      console.log(error.message);
-      alert(error.message);
-    }
+    return await request(`${BASE_URL}/category/${category}/menu`);
   },
-  putChnagedMenuName: async (category, menuName, menuId) => {
-    try {
-      await fetch(`${BASE_URL}/category/${category}/menu/${menuId}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name: menuName }),
-      });
-    } catch (error) {
-      alert(error.message);
-    }
+  putChnagedMenuName: async (category, name, menuId) => {
+    return await request(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      HTTP_METHOD.PUT({ name })
+    );
   },
   deleteMenu: async (category, menuId) => {
-    try {
-      await fetch(`${BASE_URL}/category/${category}/menu/${menuId}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    } catch (error) {
-      alert(error.message);
-    }
+    return await requestWithOutJson(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      HTTP_METHOD.DELETE()
+    );
   },
   putMenuSoldout: async (category, menuId) => {
-    try {
-      await fetch(`${BASE_URL}/category/${category}/menu/${menuId}/soldout`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    } catch (error) {
-      alert(error.message);
-    }
+    return await request(
+      `${BASE_URL}/category/${category}/menu/${menuId}/soldout`,
+      HTTP_METHOD.PUT()
+    );
   },
 };
 
